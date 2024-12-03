f1386c9da332ac134cb5cb6100b26efca30530fc
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2024-12-03 16:23:20 -0300

    atualizar rotas

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,39 @@ import { login } from "../controllers/authController.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login
+ *     tags: [Auth]
+ *     security: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               login_email:
+ *                 type: string
+ *                 description: Email do usuário
+ *               password:
+ *                 type: string
+ *                 description: Senha do usuário
+ *             required:
+ *               - login_email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Usuários obtidos com sucesso
+ *       400:
+ *         description: Erro de validação
+ *       401:
+ *         description: Credenciais inválidas
+ *       500:
+ *         description: Erro do servidor
+ */
 router.post("/login", login);
-//router.post("/register", register);
 
 export default router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,41 @@
 import { Router } from "express";
 import {
-  createUser,
   getAllUsers,
   updateUser,
+  getUserById,
+  deleteUser,
+  createUser,
 } from "../controllers/userController.js";
+import { protect, restrictTo } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/", createUser);
-router.get("/", getAllUsers);
-router.put("/:id", updateUser);
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Obter todos usuários
+ *     tags: [Users]
+ *     security:
+ *       - ApiKeyAuth: []
+ *     responses:
+ *       200:
+ *         description: Usuários obtidos com sucesso
+ *       401:
+ *         description: Token inválido ou expirado
+ *       403:
+ *         description: Você não tem permissão para realizar esta ação
+ *       500:
+ *         description: Erro do servidor
+ */
+router.get("/", protect, restrictTo("admin"), getAllUsers);
+
+router.post("/", protect, restrictTo("admin"), createUser);
+
+router.get("/:id", protect, restrictTo("admin"), getUserById);
+
+router.put("/:id", protect, restrictTo("admin"), updateUser);
+
+router.delete("/:id", protect, restrictTo("admin"), deleteUser);
 
 export default router;
