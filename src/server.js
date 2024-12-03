84fded17305d373f3b5f43ba43ad19c73ed24fc5
João Gabriel da Conceição Santos <gabrieudev@gmail.com>
2024-12-03 18:42:15 -0300

    atualizar server.js com as rotas criadas

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { sequelize } from "./config/database.js";
 import userRoutes from "./routes/userRoutes.js";
+import aircraftRoutes from "./routes/aircraftRoutes.js";
+import flightRoutes from "./routes/flightRoutes.js";
+import passengerRoutes from "./routes/passengerRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import corsOptions from "./config/cors.js";
 import swaggerSpec from "./config/swaggerConfig.js";
@@ -15,10 +18,13 @@ const port = process.env.PORT || 3000;
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/aircrafts", aircraftRoutes);
+app.use("/flights", flightRoutes);
+app.use("/passengers", passengerRoutes);
 
 async function startServer() {
   try {
